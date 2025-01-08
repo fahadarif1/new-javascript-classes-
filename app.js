35d61f0f881601b9216e5b771a6bdd634c83520b
Fahad-Arif <134074010+fahadarif1@users.noreply.github.com>
2025-01-07 23:36:54 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// class Point {
+
+// this.{}
+
+//     a ;
+//     b ; 
+//     c ;
+
+
+//     constructor(a,b,c){
+//   this.a = a;
+//   this.b = b;
+//  this.c = c ;
+//  }
+//          this ={
+//             a : value 
+//             b : value
+
+//          }
+// }
+
+// const pt = new Point ( 2, 3, 5);
+// const pt1 = new Point (7, 9);
+// const  pt2 = new Point (4, 8);
+
+// console.log(pt)
+// console.log(pt1);
+// console.log(pt2);
+
